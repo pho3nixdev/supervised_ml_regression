f1be9573c2c2955db2d3281506768948cc2ab462
pho3nixdev <javier.gonzalezgon@gmail.com>
2025-08-11 15:17:37 +0200

    Add training data for multiple variable linear regression model

--- multiple_variable_lineal_regression.py
+++ multiple_variable_lineal_regression.py
@@ -3,3 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('./deeplearning.mplstyle')
 np.set_printoptions(precision=2)
+
+X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
+y_train = np.array([460, 232, 178])
